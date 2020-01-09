c7ebd42bf2d707b65cdcd2f86cf5bedc12e1a4e9
andremoniy <andremoniy@gmail.com>
2020-01-09 22:09:44 +0100

    add test for URL connection

--- src/test/java/com/github/andremoniy/apacheclienttest/FailoverTest.java
+++ src/test/java/com/github/andremoniy/apacheclienttest/FailoverTest.java
@@ -1,28 +1,50 @@
 package com.github.andremoniy.apacheclienttest;
 
+import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.jupiter.api.Test;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FailoverTest {
 
     @Test
-    void shouldFailoverToAnotherIp() throws IOException {
+    void shouldFailoverToAnotherIpWithApacheClient() throws IOException {
         // Given
 
         // When
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             HttpGet httpget = new HttpGet("https://www.twitter.com");
             try (CloseableHttpResponse response = httpclient.execute(httpget)) {
-                System.out.println(response.getStatusLine());
+                // Then
+                final StatusLine statusLine = response.getStatusLine();
+                System.out.println(statusLine);
+                assertEquals(200, statusLine.getStatusCode());
             }
         }
+    }
 
-        // Then
+    @Test
+    void testFailoverWithURL() throws IOException {
+        // Given
+        final URL url = new URL("https://www.twitter.com");
+        final HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+
+        // When
+        urlConnection.connect();
+
+        final int responseCode = urlConnection.getResponseCode();
+        System.out.println(responseCode);
+        assertEquals(200, responseCode);
     }
 
 }
